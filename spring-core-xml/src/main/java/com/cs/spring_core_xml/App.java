0c56e04340882da773f17aa23b4948a1ce6d6bf1
sreedhark-paycorp <sreedhark-paycorp>
2024-03-24 13:19:50 +0530

    spring project

---
+++ spring-core-xml/src/main/java/com/cs/spring_core_xml/App.java
@@ -0,0 +1,13 @@
+package com.cs.spring_core_xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
